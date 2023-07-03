7635ec9bcddaae50e7f8d487b01bcb013f278c26
Pop Iulian <iulian.pop03@e-uvt.ro>
2023-07-03 17:13:32 +0300

    Final Sesiune 1

--- football-NRO/src/test/java/com/nro/footballnro/FootballNroApplicationTests.java
+++ football-NRO/src/test/java/com/nro/footballnro/FootballNroApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nro.footballmanager;
+package com.nro.footballnro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FootballManagerApplicationTests {
+class FootballNroApplicationTests {
 
 	@Test
 	void contextLoads() {
